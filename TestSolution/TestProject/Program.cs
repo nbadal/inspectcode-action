6e6cd1c6fba0708ba6512994d582c24057aca268
Nick Badal <me@nbad.al>
2020-04-12 14:14:30 -0700

    Add test project

---
+++ TestSolution/TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Test Solution!");
+        }
+    }
+}
