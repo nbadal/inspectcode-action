32aab8ef11be28c46001bcae04ef31986e8e6828
Nick Badal <me@nbad.al>
2021-05-03 14:25:20 -0700

    Add .net 5 test project

---
+++ TestSolution/TestProject5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProject5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
